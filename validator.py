4f797214af89407783a125a20ed529547ecb733a
Akashdeep Dhar <49605954+t0xic0der@users.noreply.github.com>
2019-11-03 06:45:31 +0000

    Password strength evaluator initialized

--- validator.py
+++ validator.py
@@ -31,11 +31,26 @@ def vld_lname(strpiece):
         return 0
 
 def vld_sepin(strpiece):
-    regex = '^[0-9]{6}$'
+    regex = "^[0-9]{6}$"
     if re.search(regex, strpiece):
         return 1
     else:
         return 0
 
 def vld_paswd(strpiece):
-    return 1
+    regex_ezy = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+    regex_med = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+    regex_hrd = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+    # Minimum eight characters, at least one letter and one number
+    # Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
+    # Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    if re.match(regex_ezy, strpiece):
+        if re.findall(regex_med, strpiece):
+            if re.findall(regex_hrd, strpiece):
+                return 3
+            else:
+                return 2
+        else:
+            return 1
+    else:
+        return 0

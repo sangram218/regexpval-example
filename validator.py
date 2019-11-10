2b614c723ce6742feb6196b46cc1262dfc5c04b1
sangram218 <57584287+sangram218@users.noreply.github.com>
2019-11-10 22:31:15 +0530

    Update validator.py

--- validator.py
+++ validator.py
@@ -58,6 +58,7 @@ def vld_paswd(strpiece):
 '''
 
 def vld_paswd(strpiece):
+    '''
     regex_ezy = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
     regex_med = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
     regex_hrd = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
@@ -78,3 +79,30 @@ def vld_paswd(strpiece):
         return 1
     else:
         return 0
+    ''' 
+    regweak= r"^(?=.*[a-z])(?=.*\d)[a-z\d]{8,}$" #minimum eight characters, at least one lowercase letter and one number
+    regweak1= r"^(?=.*[A-Z])(?=.*\d)[A-Z\d]{8,}$" #Minimum eight characters, at least one uppercase letter and one number
+    regmedium= r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"#Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+    regmedium1= r"^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[a-z\d@$!%*?&]{8,}$" #Minimum eight characters, at least one number and lowercase letter and one special character
+    regmedium2= r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Z\d@$!%*?&]{8,}$" #Minimum eight characters, at least one number and uppercase letter and one special character
+    regstrong = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" #Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+                           
+    mats = re.search(regstrong, strpiece) 
+    matw = re.search(regweak, strpiece)
+    matw1 = re.search(regweak1, strpiece)
+    matm= re.search(regmedium, strpiece)
+    matm1= re.search(regmedium1, strpiece)
+    matm2= re.search(regmedium2, strpiece)
+       
+    if mats: 
+        return 3
+    elif matm or matm1 or matm2: 
+        return 2
+    elif matw or matw1:
+        return 1
+    else:
+        return 0       
+      
+
+    
+    
